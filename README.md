627fd7b3ecc6ca8a03af2f03ba673acf50490178
Amalie Von Behren <vbamalie@gmail.com>
2022-04-07 22:14:09 -0400

    updated read me

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Todo-list
 to do list homework
+
+## Day planner
+
+this homework assignment instructed us to make a day planner. the header displays the date and current time.
+
+The day is split into nine hour long time blocks. whatever time block you are currently in is red while the times that have already past are grey and the times that are still to come are green.
+
+![Screen shot of day planner2](./assets/image/Screen%20Shot%202022-04-07%20at%2012.11.00%20PM.png)
+
+![Screen shot of day planner](./assets/image/Screen%20Shot%202022-04-07%20at%2012.10.53%20PM.png)
+
+
+If you click the save button, it will save the text into local storage so that if you reload the page, it will stick around.
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//the time setter 
+//this is used to compare what hour it is versus the time blocks
 var currentHour = moment().format("H");
 var hourNine = 9;
 var hourTen =10;
@@ -10,13 +10,14 @@ var hourThree = 15;
 var hourFour = 16;
 var hourFive = 17
 
-
+//this is the time in the header. it checks if the minute has changed every second
 setInterval(()=>{
     var currentTime = (moment().format('MMMM Do YYYY, h:mm a'));
     $("#currentDay").text(currentTime);
     return currentTime
 }, 1000);
 
+//this stores edited text in the text blocks into local files when the save button is pressed
 $(document).ready(function(){
     $(".saveBtn").on("click", function(){
         var value = $(this).siblings(".information").val();
@@ -24,7 +25,7 @@ $(document).ready(function(){
         localStorage.setItem(time , value);
 })
 });
-
+//this is the function to change the time block's colors into the past present or future
 function colorChanger(hourBlock){
     if (currentHour == hourBlock){
         var change = "present"
@@ -40,6 +41,7 @@ function colorChanger(hourBlock){
     }
 };
 
+//this calls the function to change the time colors
 $("#hour-nine").children(".information").addClass(colorChanger(hourNine));
 $("#hour-ten").children(".information").addClass(colorChanger(hourTen));
 $("#hour-eleven").children(".information").addClass(colorChanger(hourEleven));
@@ -50,6 +52,8 @@ $("#hour-three").children(".information").addClass(colorChanger(hourThree));
 $("#hour-four").children(".information").addClass(colorChanger(hourFour));
 $("#hour-five").children(".information").addClass(colorChanger(hourFive));
 
+
+//this calls the function to store into local storage
 $("#hour-nine .information").val(localStorage.getItem("hour-nine"));
 $("#hour-ten .information").val(localStorage.getItem("hour-ten"));
 $("#hour-eleven .information").val(localStorage.getItem("hour-eleven"));
